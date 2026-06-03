43ac9670ca000373837e8dc6c16299adbb9584a5
Vivek-4142 <beheravivek02@gmail.com>
2026-06-03 13:56:53 +0530

    reverse polish rotation solved

---
+++ stack'/evaluate_reverse_polish_rotation.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> st = new Stack<>();
+
+        for(String ch : tokens) {
+
+            if(ch.equals("+")) {
+                int a = st.pop();
+                int b = st.pop();
+                st.push(b + a);
+
+            } else if(ch.equals("-")) {
+                int a = st.pop();
+                int b = st.pop();
+                st.push(b - a);
+
+            } else if(ch.equals("*")) {
+                int a = st.pop();
+                int b = st.pop();
+                st.push(b * a);
+
+            } else if(ch.equals("/")) {
+                int a = st.pop();
+                int b = st.pop();
+                st.push(b / a);
+
+            } else {
+                st.push(Integer.parseInt(ch));
+            }
+        }
+
+        return st.peek();
+    }
+}
